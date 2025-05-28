a4924e075292d2e78fa898fc6f5d09879e2cc9c2
djangopepper <fr3d.mobile@gmail.com>
2025-05-28 17:02:37 +0200

    doublon loin

--- relevage/src/index.tsx
+++ relevage/src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Utilisez 'react-dom/client' pour React 18
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store';
 import TableManager from './components/TableManager';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <TableManager />
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement); // Créez un root avec React 18
+    root.render(
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <TableManager />
+            </PersistGate>
+        </Provider>
+    );
+}
